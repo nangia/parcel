a80c0f8269f398eb61e4328ee45a271b90a6b8a1
Sandeep Nangia <sandeep@nangia.in>
2025-01-09 12:46:48 +0530

    Working postage calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weight, setWeight] = useState("");
+  const [postage, setPostage] = useState<string | null>(null);
+
+  const weightToPostage = (weightInKg: number) => {
+    const numOf500Gms = Math.ceil(weightInKg / 0.5);
+    console.log();
+    const remaining500Gms = Math.max(numOf500Gms - 1, 0);
+    const cost = 1.18 * (19 + 16 * remaining500Gms + 17);
+    return Math.round(cost * 100) / 100;
+  };
+
+  const handleSubmit = (/*e: React.FormEvent<HTMLFormElement>*/) => {
+    /* e.preventDefault(); */
+    const max_weight = 35;
+    const bo_max_weight = 10;
+    const weightFloat = parseFloat(weight);
+    if (isNaN(weightFloat) || weightFloat < 0) {
+      setPostage("Invalid weight");
+    } else if (weightFloat > max_weight) {
+      setPostage("Weight too high. Keep it below " + max_weight + "kg");
+    } else {
+      const calculatedPostage = weightToPostage(weightFloat);
+      if (weightFloat > bo_max_weight) {
+        setPostage(
+          "The required postage is Rs." +
+            calculatedPostage +
+            ". (Cannot be sent from BO. Go to GPO)."
+        );
+      } else {
+        setPostage(
+          "The required postage is Rs." +
+            calculatedPostage +
+            "(Can be done from BO)"
+        );
+      }
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="form-group">
+      <h1>India Post Registered Parcel Charge Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="weight">Enter weight of package in kgs:</label>
+        <input
+          type="number"
+          step="0.01"
+          id="weight"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleSubmit}
+        >
+          Calculate Postage
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+      {postage && <h3>{postage}</h3>}
+    </div>
+  );
 }
 
-export default App
+export default App;
